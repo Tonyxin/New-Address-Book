12eae854f4516999195c4998cdef2dab04042fe8
Tonyxin <u201214149@hust.edu.cn>
2015-11-13 13:52:28 +0800

    add solution

---
+++ Address Book/Address Book.h
@@ -0,0 +1,32 @@
+
+// Address Book.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CAddressBookApp: 
+// 有关此类的实现，请参阅 Address Book.cpp
+//
+
+class CAddressBookApp : public CWinApp
+{
+public:
+	CAddressBookApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CAddressBookApp theApp;
